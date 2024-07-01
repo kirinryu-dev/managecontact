5e441212e1efebe745ad3c4d323f107f2c068db5
kirinryu.dev <kirinryu.dev@gmail.com>
2024-07-01 08:37:49 +0000

    a new week starty

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Contact from './pages/Contact';
 import Root from './utils/root';
 import Error from './error/error-page';
 
